67fdd8f163877f7430f5c0fc9851522215276e6f
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2019-12-18 21:05:06 -0300

    Rotas de navegação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,8 +18,12 @@ class MyApp extends StatelessWidget {
           textTheme: ThemeData.light().textTheme.copyWith(
               body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              title: TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'))),
-      home: CategoriesScreen(),
+              title: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold))),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
